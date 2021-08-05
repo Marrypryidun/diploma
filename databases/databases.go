3f94d272337778c709e7ab39e48472a4d52b62a6
Marry <marrypryidun@gmail.com>
2021-08-05 18:45:40 +0300

    gitignore

---
+++ databases/databases.go
@@ -0,0 +1,41 @@
+package databases
+
+import (
+	"diplom/config"
+	"fmt"
+	"gopkg.in/mgo.v2"
+)
+
+// Database shares global database instance
+var (
+	Database MongoDB
+)
+
+// MongoDB manages MongoDB connection
+type MongoDB struct {
+	MgDbSession *mgo.Session
+}
+
+// Init initializes mongo database
+func (db *MongoDB) Init() error {
+	dialUrl := config.Config.MgAddrs()
+
+	// Create a session which maintains a pool of socket connections
+	// to the DB MongoDB database.
+	var err error
+	db.MgDbSession, err = mgo.Dial(dialUrl)
+
+	if err != nil {
+		fmt.Println("Can't connect to mongo, go error: ", err)
+		return err
+	}
+
+	return err
+}
+
+// Close the existing connection
+func (db *MongoDB) Close() {
+	if db.MgDbSession != nil {
+		db.MgDbSession.Close()
+	}
+}
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"diplom/config"
+	"diplom/databases"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	//ttpl "github.com/night-codes/go-gin-ttpl"
 )
 
 func main() {
+	if err := config.LoadConfig(); err != nil {
+		fmt.Println("config.LoadConfig() err:", err.Error())
+		return
+	}
+	if err := databases.Database.Init(); err != nil {
+		fmt.Println("databases.Database.Init() err:", err.Error())
+		return
+	}
+	defer databases.Database.Close()
+
 	gin.SetMode(gin.ReleaseMode)
 	router = gin.New()
 	router.Static("/css", "./templates/css")
